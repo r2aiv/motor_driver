a43b537292af401b69686da34a8e375c99706e26
Denis Sovkov <l0calhost@mail.ru>
2017-08-29 11:58:23 +0300

    softstart

--- Firmware/pwm_motor_ctrl/Src/main.c
+++ Firmware/pwm_motor_ctrl/Src/main.c
@@ -177,11 +177,12 @@ int main(void)
 		if((SensorState & 0x01) == 0x01) // Прожектор вверху
 		{
 			ConsoleWrite("Projector is UP!\r");
+			//if((SensorState & 0x02)==0x02) break; // Лифт вверху - выходим
 			if((SensorState & 0x04)==0x04) // Нижний концевик активен
 			{
 				ConsoleWrite("Lift is down, start moving UP\r\n");
 				// TODO: подъем
-				
+				PWMValue=MySettings.ForwardSpeed;
 				DevState=STATE_FORWARD; //Едем вверх
 				while(PWMValue++ <= PWMSetting-2) delay_ms(MySettings.SoftStartDelay);		// Плавный пуск
 				while((GetSensorState() & 0x04)==0x04); // Ждем схода с концевика
@@ -196,8 +197,10 @@ int main(void)
 		
 		if((SensorState & 0x01) == 0x00) //Прожектор внизу			
 		{
+			PWMValue=MySettings.BackwardSpeed;
 			ConsoleWrite("Projector is DOWN!\r");
-			if((SensorState & 0x02) == 0x02) // Верхний концевик активен
+			//if((SensorState & 0x04)==0x04) break; // Лифт внизу - выходим
+			if((SensorState & 0x02)==0x02) // Верхний концевик активен
 			{
 				// TODO: Опускание
 			
